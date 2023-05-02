6ee32baa72740eba3f720b98ff622f37d7d8780c
Amauari Ulloa Galicia <ing.amaury06@gmail.com>
2023-05-02 12:55:18 -0400

    app-vs2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -41,7 +41,7 @@ function Product({id, title, price, description, category, image}) {
             {Array(rating)
             .fill()
             .map((_, i) => (
-            <StarIcon className="h-5 text-yellow-500 "/>       
+            <StarIcon className="h-5 text-yellow-500 " key={id}/>       
             ))}    
         </div>
         <p className="text-xs mt-2 my-2 line-clamp-2">{description}</p>
